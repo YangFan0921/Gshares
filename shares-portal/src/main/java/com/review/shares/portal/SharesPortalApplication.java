40dd41a95c9fbb76831bd89a6ada940bbcacbc7d
aFan <2388408281@qq.com>
2022-08-09 13:11:32 +0800

    Initial commit

---
+++ shares-portal/src/main/java/com/review/shares/portal/SharesPortalApplication.java
@@ -0,0 +1,13 @@
+package com.review.shares.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesPortalApplication.class, args);
+    }
+
+}
---
+++ shares-portal/src/test/java/com/review/shares/portal/SharesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.shares.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharesPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
