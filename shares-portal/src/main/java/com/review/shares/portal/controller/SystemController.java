b97192ae00c546c6483d3ce310529971189617a0
aFan <2388408281@qq.com>
2022-08-13 17:21:02 +0800

    注册验证

--- shares-portal/src/main/java/com/review/shares/portal/controller/SystemController.java
+++ shares-portal/src/main/java/com/review/shares/portal/controller/SystemController.java
@@ -1,10 +1,11 @@
 package com.review.shares.portal.controller;
 
-import com.baomidou.mybatisplus.extension.api.R;
 import com.review.shares.portal.service.IUserService;
 import com.review.shares.portal.vo.RegisterVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,16 @@ public class SystemController {
     @Autowired
     private IUserService userService;
 
+
+    //@Validated启用Spring Validation框架对RegisterVo参数进行验证
+    //BindingResult保存验证结果
     @PostMapping("/register")
-    public String RegisterStudent(RegisterVo registerVo){
+    public String RegisterStudent(@Validated RegisterVo registerVo, BindingResult result){
         log.debug("接受到的表单信息：{}",registerVo);
+        if (result.hasErrors()){
+            String msg = result.getFieldError().getDefaultMessage();
+            return msg;
+        }
         try {
             userService.registerStudent(registerVo);
             return "注册完成！";
--- shares-portal/src/main/java/com/review/shares/portal/vo/RegisterVo.java
+++ shares-portal/src/main/java/com/review/shares/portal/vo/RegisterVo.java
@@ -2,14 +2,29 @@ package com.review.shares.portal.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 //V(Value)o(object) vo:值对象
 // 用于封装项目开发过程中一些数据
 @Data
 public class RegisterVo {
     //与前端页面中的name值一致
+    @NotBlank(message = "邀请码不能为空")
     private String inviteCode;
+
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1\\d{10}$",message = "手机号码格式不正确")
     private String phone;
+
+    @NotBlank(message = "昵称不能为空")
+    @Pattern(regexp = "^.{2,20}$",message = "昵称长度为2-20")
     private String nickname;
+
+    @NotBlank(message = "密码不能为空")
+    @Pattern(regexp = "^\\w{6,20}$",message = "密码长度为6-20")
     private String password;
+
+    @NotBlank(message = "确认密码不能为空")
     private String confirm;
 }
